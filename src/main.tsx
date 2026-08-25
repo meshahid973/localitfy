13678150c36f529ce837f39a4a36a83000e9b8b0
meshahid.973 <141939170+meshahid973@users.noreply.github.com>
2026-08-25 15:46:03 +0300

    perf: load canonical performance CSS after app styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AppErrorBoundary } from "./app/AppErrorBoundary";
+import "./features/shell/performance.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <AppErrorBoundary>
