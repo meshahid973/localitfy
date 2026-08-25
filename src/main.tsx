b0ea6e1fdacf3da5def8b458add5bd558487ebea
meshahid.973 <141939170+meshahid973@users.noreply.github.com>
2026-08-25 18:23:23 +0300

    style: make home.css the sole home owner

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AppErrorBoundary } from "./app/AppErrorBoundary";
-import "./features/shell/release.css";
 import "./features/shell/performance.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
