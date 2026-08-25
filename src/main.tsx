e325275f2083950afc433f6fd2bdca1e13b30b87
meshahid.973 <141939170+meshahid973@users.noreply.github.com>
2026-08-25 17:47:04 +0300

    Load canonical release and performance CSS last

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AppErrorBoundary } from "./app/AppErrorBoundary";
+import "./features/shell/release.css";
 import "./features/shell/performance.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -9,4 +10,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <App />
   </AppErrorBoundary>
 );
-
