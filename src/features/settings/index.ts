613ab6000237866e7b58e1c0cb2226023151ca05
meshahid.973 <141939170+meshahid973@users.noreply.github.com>
2026-08-25 11:35:37 +0300

    settings: keep lazy content out of static barrel

--- src/features/settings/index.ts
+++ src/features/settings/index.ts
@@ -1,4 +1,3 @@
-export { default as SettingsCategoryContent } from "./SettingsCategoryContent";
 export type * from "./settings.types";
 export type * from "./theme.types";
 export * from "./settings.constants";
